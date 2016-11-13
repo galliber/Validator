912baba7ca0ba7a856ca002656822ba197365299
galliber <galiber12321@gmail.com>
2016-11-14 00:42:12 +0200

    Update EmailValidator.java

--- EmailValidator.java
+++ EmailValidator.java
@@ -4,7 +4,7 @@ public class EmailValidator
 {
 	private Pattern pattE;
 	private Matcher matchE;
-	private static final String EMAIL_PATTERN="[a-z][a-zA-Z0-9\\.\\-]+@[a-zA-Z0-9]+\\.[a-z]{2,4}";
+	private static final String EMAIL_PATTERN="^[a-z][a-zA-Z0-9\\.\\-]+@[a-zA-Z0-9]+\\.[a-z]{2,4}$";
 	
 	public EmailValidator()
 	{
